c4ba9e42730541d32e6db9523d3644d77b68042c
neomichael <talentmichael123@gmail.com>
2025-05-04 12:54:58 +0800

    upda te
    update

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// filepath: c:\Users\micli\Documents\GitHub\contactsmaintenance\sakila-actor-crud\webpack.config.js
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/, // Process .js and .jsx files
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+            },
+          },
+        ],
+      },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8080,
+    },
+};
